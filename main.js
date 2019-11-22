75a55a2d3fd0ff65cb73cf96dac2604f24544f55
haniejuraimi <haniealia08@gmail.com>
2019-11-22 11:02:15 +0800

    Added Firebase
    Instead of listing out in Js, we changed to Firebase to store all the data

--- main.js
+++ main.js
@@ -1,334 +1,170 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCtd_uNBZMvw8XRcKog7aGd6UNodtEZCwo",
+    authDomain: "coupons-for-you-el.firebaseapp.com",
+    databaseURL: "https://coupons-for-you-el.firebaseio.com",
+    projectId: "coupons-for-you-el",
+    storageBucket: "coupons-for-you-el.appspot.com",
+    messagingSenderId: "950396480131",
+    appId: "1:950396480131:web:057ca2d53a1d57ab6709b6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 var app = new Vue({
     el: '#app',
     data: {
-        coupons: [
-            {
-                companyName: 'KFC',
-                code: '2CHICKEN',
-                price: '$4.95',
-                couponName: '2pc Chicken Meal',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "kfc_5.JPG"
-            },
-            {
-                companyName: 'KFC',
-                code: '2TENDERS',
-                price: '$1.95',
-                couponName: '2pc Hot & Crispy Tenders',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1275.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: '8FOR8',
-                price: '$8.00',
-                couponName: '3pc Chicken & 5 Tenders',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1276.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'NUGGETS',
-                price: '$3.00',
-                couponName: '9pc Nuggets',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1277.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'ANY2BOWLS',
-                price: '$5.00',
-                couponName: 'Any 2 Bowls',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1278.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: '2WINGS',
-                price: '$3.35',
-                couponName: '1-for-1 Wings',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1279.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'STACKER',
-                price: '$7.95',
-                couponName: 'Zinger Stacker & Cheese Fries',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1282.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'SNACKER',
-                price: '$2.50',
-                couponName: '4pc Nuggets with Fries',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1283.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'BWBUDDYML',
-                price: '$9.95',
-                couponName: 'Bowl Buddy Meal',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1284.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'PKTBUDDYML',
-                price: '$9.95',
-                couponName: 'BBQ Pockett Buddy Meal',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1285.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'SNACKNSHARE',
-                price: '$8.95',
-                couponName: 'Snack & Share Box (5 Tenders & 12 Nuggets)',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1286.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'FEAST',
-                price: '$25.95',
-                couponName: 'Savers Feast (9pc Chicken, 2 Mashed Potatoes, 1 Coleslaw, 4 Regular Soft Drinks',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1287.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'TWISTER',
-                price: '$4',
-                couponName: '1-for-1 Twister',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1288.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'MILO',
-                price: '$3.30',
-                couponName: '1-for-1 Iced Milo',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1289.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'RISER',
-                price: '$3.95',
-                couponName: 'Riser Meal',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1290.jpg"
-            },
-            {
-                companyName: 'KFC',
-                code: 'PORRIDGE',
-                price: '$4.95',
-                couponName: 'Original Recipe Porridge Meal',
-                dueDate: '19 Nov 2019',
-                invalidPlaces: 'KFC Jewel Changi Airport, Kidzania and Singapore Zoo',
-                invalidThings: 'KFC Delivery and KFC Corporate Events',
-                gambar: "IMG_1291.jpg"
-            },
+        coupons: [],
+        oldCoupons: [
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$5.50',
                 couponName: 'Turkey Ham Croissanwich w/ Egg Meal',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1322.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$7.00',
                 couponName: 'Whopper Meal',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1321.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$5.00',
                 couponName: 'Long Chicken Set',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1320.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$5.00',
                 couponName: 'Tendergrill Chicken Set',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1319.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$4.00',
                 couponName: ' Hamburger Set',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1318.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$3.00',
                 couponName: 'Turkey Sausage Croissanwich With Egg',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1317.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$6.00',
                 couponName: 'Turkey Ham & Cheese Omelette Platter',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1316.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$3.20',
                 couponName: '1-For-1 Minute Maid Refresh',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1315.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$1.00',
                 couponName: 'Hash Browns',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1314.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$1.50',
                 couponName: 'Onion Rings',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1313.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$1.50',
                 couponName: '4pc Nuggets',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1312.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$11.00',
                 couponName: 'Double Fish N Crisp + Single Mushroom Swiss Set',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1311.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$15.90',
                 couponName: 'Mushroom Swiss Tendergrill Chicken + TenderCrisp Chicken + Hershey Sundae Pie Set',
                 dueDate: '25 Feb 2020',
-                invalidPlaces: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "IMG_1310.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$6.40',
                 couponName: '1-for-1 Whopper (ALA CARTE)',
                 dueDate: '-',
-                invalidPlaces: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
                 invalidThings: 'BK Delivery and Catering  ',
                 gambar: "whop.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$4.00',
                 couponName: '2 for $4 (Cheeseburger / BBQ Chick N Crisp',
                 dueDate: '-',
-                invalidPlaces: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
                 invalidThings: 'during weekends, bulk orders, BK Delivery and Catering  ',
                 gambar: "2 for 4.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$5.00',
                 couponName: 'Flamin Hot Deals',
                 dueDate: '-',
-                invalidPlaces: '-',
+                description: '-',
                 invalidThings: 'BK Delivery and Catering and bulk orders',
                 gambar: "$5.jpg"
             },
             {
                 companyName: 'BURGER KING',
-                code: '-',
                 price: '$2+',
                 couponName: 'Rise & Smile',
                 dueDate: '-',
-                invalidPlaces: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
                 invalidThings: 'during weekends, BK Delivery and Catering  ',
                 gambar: "rise.jpg"
             },
@@ -336,8 +172,17 @@ var app = new Vue({
         selectedCompanyName: null,
         selectedPrice: null,
     },
+    mounted: async function() {
+        const db = firebase.firestore();
+        let couponsRef = db.collection('coupons');
 
+        const snapshot = await couponsRef.get();
+        this.coupons = snapshot.docs.map(doc => doc.data());
+    },
     methods: {
+        printCoupons: function() {
+            console.log(this.coupons.length);
+        },
         companyNameFilterSelected: function (selectedCompanyName) {
             this.selectedCompanyName = selectedCompanyName;
         },
@@ -363,18 +208,17 @@ var app = new Vue({
             });
         },
     },
-
     computed: {
         filteredCoupons: function () {
             const selectedPrice = this.selectedPrice;
             const selectedCompanyName = this.selectedCompanyName;
 
             // price is null, companyName is KFC
-            console.log({selectedPrice, selectedCompanyName})
+            console.log({ selectedPrice, selectedCompanyName })
 
 
             let couponsFilteredByPrice = this.coupons;
-            if(selectedPrice){
+            if (selectedPrice) {
                 // filter
                 couponsFilteredByPrice = this.coupons.filter(function (coupon) {
                     if (coupon.price === selectedPrice) {
@@ -385,12 +229,12 @@ var app = new Vue({
                 });
             }
 
-            if(!selectedCompanyName){ 
-                return couponsFilteredByPrice;   
+            if (!selectedCompanyName) {
+                return couponsFilteredByPrice;
             }
 
             console.log("at company name filter, the value of coupons filtered by price", couponsFilteredByPrice)
-            const couponsFilteredByPriceAndCompanyName = couponsFilteredByPrice.filter(function(coupon){
+            const couponsFilteredByPriceAndCompanyName = couponsFilteredByPrice.filter(function (coupon) {
                 if (coupon.companyName === selectedCompanyName) {
                     return true;
                 } else {
@@ -401,13 +245,13 @@ var app = new Vue({
 
             return couponsFilteredByPriceAndCompanyName;
         },
-        couponsWithUniqueCompanyNames: function() {
-            function getUnique(coupons){
+        couponsWithUniqueCompanyNames: function () {
+            function getUnique(coupons) {
                 var uniqueArrayOfCompanies = [];
-                
+
                 // Loop through array values
-                for(const coupon of coupons){
-                    if(!uniqueArrayOfCompanies.includes(coupon.companyName)){
+                for (const coupon of coupons) {
+                    if (!uniqueArrayOfCompanies.includes(coupon.companyName)) {
                         uniqueArrayOfCompanies.push(coupon.companyName);
                     }
                 }
@@ -417,13 +261,13 @@ var app = new Vue({
             return getUnique(this.coupons);
         },
 
-        couponsWithUniquePrices: function() {
-            function getUnique(coupons){
+        couponsWithUniquePrices: function () {
+            function getUnique(coupons) {
                 var uniqueArrayOfPrices = [];
-                
+
                 // Loop through array values
-                for(const coupon of coupons){
-                    if(!uniqueArrayOfPrices.includes(coupon.price)){
+                for (const coupon of coupons) {
+                    if (!uniqueArrayOfPrices.includes(coupon.price)) {
                         uniqueArrayOfPrices.push(coupon.price);
                     }
                 }
@@ -433,6 +277,6 @@ var app = new Vue({
             return getUnique(this.coupons);
         }
     }
-    
+
 
 })
