c09107271356a785bb6b0eb371a147c827fb128a
haniejuraimi <50759167+haniejuraimi@users.noreply.github.com>
2019-12-20 14:22:20 +0800

    Removed array of old coupons
    As we were using Firebase to store our data, I deleted the old array

--- main.js
+++ main.js
@@ -15,159 +15,159 @@ var app = new Vue({
     data: {
         coupons: [],
         oldCoupons: [
-            {
-                companyName: 'BURGER KING',
-                price: '$5.50',
-                couponName: 'Turkey Ham Croissanwich w/ Egg Meal',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1322.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$7.00',
-                couponName: 'Whopper Meal',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1321.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$5.00',
-                couponName: 'Long Chicken Set',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1320.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$5.00',
-                couponName: 'Tendergrill Chicken Set',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1319.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$4.00',
-                couponName: ' Hamburger Set',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1318.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$3.00',
-                couponName: 'Turkey Sausage Croissanwich With Egg',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1317.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$6.00',
-                couponName: 'Turkey Ham & Cheese Omelette Platter',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1316.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$3.20',
-                couponName: '1-For-1 Minute Maid Refresh',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1315.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$1.00',
-                couponName: 'Hash Browns',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1314.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$1.50',
-                couponName: 'Onion Rings',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1313.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$1.50',
-                couponName: '4pc Nuggets',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1312.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$11.00',
-                couponName: 'Double Fish N Crisp + Single Mushroom Swiss Set',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1311.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$15.90',
-                couponName: 'Mushroom Swiss Tendergrill Chicken + TenderCrisp Chicken + Hershey Sundae Pie Set',
-                dueDate: '25 Feb 2020',
-                description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "IMG_1310.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$6.40',
-                couponName: '1-for-1 Whopper (ALA CARTE)',
-                dueDate: '-',
-                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
-                invalidThings: 'BK Delivery and Catering  ',
-                gambar: "whop.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$4.00',
-                couponName: '2 for $4 (Cheeseburger / BBQ Chick N Crisp',
-                dueDate: '-',
-                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
-                invalidThings: 'during weekends, bulk orders, BK Delivery and Catering  ',
-                gambar: "2 for 4.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$5.00',
-                couponName: 'Flamin Hot Deals',
-                dueDate: '-',
-                description: '-',
-                invalidThings: 'BK Delivery and Catering and bulk orders',
-                gambar: "$5.jpg"
-            },
-            {
-                companyName: 'BURGER KING',
-                price: '$2+',
-                couponName: 'Rise & Smile',
-                dueDate: '-',
-                description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
-                invalidThings: 'during weekends, BK Delivery and Catering  ',
-                gambar: "rise.jpg"
-            },
+            // // {
+            // //     companyName: 'Burger King',
+            // //     price: '$5.50',
+            // //     couponName: 'Turkey Ham Croissanwich w/ Egg Meal',
+            // //     dueDate: '25 Feb 2020',
+            // //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            // //     invalidThings: 'BK Delivery and Catering  ',
+            // //     image: "IMG_1322.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$7.00',
+            //     couponName: 'Whopper Meal',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1321.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$5.00',
+            //     couponName: 'Long Chicken Set',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1320.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$5.00',
+            //     couponName: 'Tendergrill Chicken Set',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1319.jpg"
+            // },
+            // // {
+            // //     companyName: 'Burger King',
+            // //     price: '$4.00',
+            // //     couponName: ' Hamburger Set',
+            // //     dueDate: '25 Feb 2020',
+            // //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            // //     invalidThings: 'BK Delivery and Catering  ',
+            // //     image: "IMG_1318.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$3.00',
+            //     couponName: 'Turkey Sausage Croissanwich With Egg',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1317.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$6.00',
+            //     couponName: 'Turkey Ham & Cheese Omelette Platter',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1316.jpg"
+            // },
+            // // {
+            // //     companyName: 'Burger King',
+            // //     price: '$3.20',
+            // //     couponName: '1-For-1 Minute Maid Refresh',
+            // //     dueDate: '25 Feb 2020',
+            // //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            // //     invalidThings: 'BK Delivery and Catering  ',
+            // //     image: "IMG_1315.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$1.00',
+            //     couponName: 'Hash Browns',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1314.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$1.50',
+            //     couponName: 'Onion Rings',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1313.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$1.50',
+            //     couponName: '4pc Nuggets',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1312.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$11.00',
+            //     couponName: 'Double Fish N Crisp + Single Mushroom Swiss Set',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1311.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$15.90',
+            //     couponName: 'Mushroom Swiss Tendergrill Chicken + TenderCrisp Chicken + Hershey Sundae Pie Set',
+            //     dueDate: '25 Feb 2020',
+            //     description: 'BK Kallang Leisure Park, Jewel and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "IMG_1310.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$6.40',
+            //     couponName: '1-for-1 Whopper (ALA CARTE)',
+            //     dueDate: '-',
+            //     description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+            //     invalidThings: 'BK Delivery and Catering  ',
+            //     image: "whop.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$4.00',
+            //     couponName: '2 for $4 (Cheeseburger / BBQ Chick N Crisp',
+            //     dueDate: '-',
+            //     description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+            //     invalidThings: 'during weekends, bulk orders, BK Delivery and Catering  ',
+            //     image: "2 for 4.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$5.00',
+            //     couponName: 'Flamin Hot Deals',
+            //     dueDate: '-',
+            //     description: '-',
+            //     invalidThings: 'BK Delivery and Catering and bulk orders',
+            //     image: "$5.jpg"
+            // },
+            // {
+            //     companyName: 'Burger King',
+            //     price: '$2+',
+            //     couponName: 'Rise & Smile',
+            //     dueDate: '-',
+            //     description: 'BK Kallang Leisure Park (on event days) and Airport Stores',
+            //     invalidThings: 'during weekends, BK Delivery and Catering  ',
+            //     image: "rise.jpg"
+            // },
         ],
         selectedCompanyName: null,
         selectedPrice: null,
